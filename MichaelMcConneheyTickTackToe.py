778f280e9155c27b59e5c277fbaf93de722b1307
Michael McConnehey <michaelgmc14@gmail.com>
2022-01-13 23:49:38 -0700

    v.5

--- MichaelMcConneheyTickTackToe.py
+++ MichaelMcConneheyTickTackToe.py
@@ -1,7 +1,10 @@
 '''
-Tick Tack Toe Game
+Tic Tac Toe Game
 Michael McConnehey
 '''
+
+from re import L
+
 
 def main():
     ''' Holds the main game loop logic
@@ -12,10 +15,15 @@ def main():
         Thanks them for playing
         return: None
     '''
-    player1 = 'x'
-    player2 = 'o'
+    player = next_player('')
     board = create_board()
+
+    while not is_winner(board) or is_draw(board):
+        display_board(board)
+        make_move(player, board)
+        player = next_player(player)
     display_board(board)
+
     # loop if there isn't a winner or if the game isn't a draw
         # display the board
 
@@ -54,22 +62,48 @@ def display_board(board):
 
 def is_draw(board):
     ''' return: True if board is a draw, False if board is still playable '''
-    pass
+    for move in range(9):
+        if board[move] != 'x' and board[move] != 'o':
+            return False
+    return True
 
 def is_winner(board):
     ''' return: True if someone won, False if there is no winner '''
-    pass
+    if board[0] + board[1] + board[2]:
+        return True
+    elif board[3] + board[4] + board[5]:
+        return True
+    elif board[6] + board[7] + board[8]:
+        return True
+    elif board[0] + board[3] + board[6]:
+        return True
+    elif board[1] + board[4] + board[7]:
+        return True
+    elif board[2] + board[5] + board[8]:
+        return True
+    elif board[0] + board[4] + board[8]:
+        return True
+    elif board[2] + board[4] + board[6]:
+        return True
+    else:
+        return False
 
 def make_move(player, board):
     ''' Prompts player to select a square to play
         Assigns the player to that board location if it is a legal move
         return: None
     '''
-    pass      
+    move = int(input(f'{player}, make your move. '))    
+    board[move-1] = player
+
 
 def next_player(current):
     ''' return: x if current is o, otherwise x '''
-    pass
+    if current == '' or current == 'o':
+        return 'x'
+    else:
+        return 'o'
+
 
 # run main if this has been called from the command line
 if __name__ == "__main__":
