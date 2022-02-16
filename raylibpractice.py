e1e9c9d64f4d40b1818e855d28efdffca5a1abaa
Michael McConnehey <michaelgmc14@gmail.com>
2022-02-16 14:28:01 -0700

    class example complete

--- raylibpractice.py
+++ raylibpractice.py
@@ -3,6 +3,6 @@ init_window(800, 450, 'Hello')
 while not window_should_close():
     begin_drawing()
     clear_background(BLACK)
-    draw_text('Hello World', 190, 200, 20, WHITE)
+    draw_text('Hello World ', 190, 200, 20, WHITE)
     end_drawing()
 close_window()
